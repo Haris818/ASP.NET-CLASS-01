56b2366c978059e4ff5a9a7d52c485a6eadc9487
Hp <Hp@DESKTOP-0EGL18S>
2024-12-16 19:38:53 +0500

    Add project files.

---
+++ Controllers/WEBController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CLASS_01.Controllers
+{
+    public class WEBController : Controller
+    {
+        // GET: WEB
+        public ActionResult HOME()
+        {
+            return View();
+        }
+    }
+}
